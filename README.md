0b9dead3bdd3566d8c6bc7366d54183ea4c85bd8
exweilt <exwelt@posteo.net>
2023-11-18 11:07:51 +0000

    big refactoring

---
+++ README.md
@@ -0,0 +1,3 @@
+## TODO:
+- works only with english youtube - `get_album_name` and `Album - ` format
+- achieve support of `music.youtube.com`
--- adl.py
+++ adl.py
@@ -1,175 +1,98 @@
-# import youtube_dl
-# import eyed3
-# from pytube import YouTube, Playlist
-
 from urllib.request import urlopen, urlretrieve
 from os.path import exists
 from os import mkdir, rename, remove
-from colorama import Fore, Back, Style
 from time import sleep
-import os
+import io
 import subprocess
 import sys
-from PIL import Image
-from mutagen.mp4 import MP4, MP4Cover
-import io
+import copy
+from dataclasses import dataclass
+
+from colorama import Fore, Back, Style
+
 import yt_dlp
+from mutagen.mp4 import MP4, MP4Cover
+from PIL import Image
 
-# eyed3.log.setLevel("ERROR")
 
-LIST_FILE='.\\list.txt'
-MUSIC_DIR='D:\\Music'
-
+LIST_FILE='./list.txt'
+MUSIC_DIR='D:/Music'
 COOKIES_FILE = "cookies_last.txt"
 
 
-def green(str):
-    print(Fore.GREEN + str + Style.RESET_ALL)
+class Console:
+    @staticmethod
+    def green(str):
+        print(Fore.GREEN + str + Style.RESET_ALL)
+
+    @staticmethod
+    def red(str):
+        print(Fore.RED + str + Style.RESET_ALL)
+
+    @staticmethod
+    def info(str):
+        print(Fore.CYAN + f"(i) {str}" + Style.RESET_ALL)
 
 
-def red(str):
-    print(Fore.RED + str + Style.RESET_ALL)
+@dataclass
+class SongOptions:
+    title:  str         = None
+    id: str             = None
+    album_foldername: str   = None
+    album_name: str     = None
+    album_id: str       = None
+    artist: str         = None
+    genre: str          = None
+    year: str           = None
+    track_number: int   = None
+    total_tracks: int   = None
 
 
-def info(str):
-    print(Fore.CYAN + f"(i) {str}" + Style.RESET_ALL)
-
-
-def getCoverUrl(playlistId):
-    with urlopen(f'https://music.youtube.com/playlist?list={playlistId}') as res:
+def get_cover_url(playlist_id):
+    with urlopen(f'https://music.youtube.com/playlist?list={playlist_id}') as res:
         body = res.read().decode('utf-8')
         key = '<meta property="og:image" content="'
         body = body[body.index(key)+len(key):]
         body = body[:body.index('">')]
         return body
 
-
-# def dlPlaylist(line, curArtist, curGenre):
-#     albumDir = f'{MUSIC_DIR}\\{line[0]}'
-#     if not exists(albumDir):
-#         mkdir(albumDir)
-#     url = f'https://music.youtube.com/playlist?list={line[1]}'
-
-#     p = Playlist(url)
-
-#     info (f"Downloading {p.title} album")
-
-#     for idx, url in enumerate(p.video_urls):
-#         info (url)
-#         yt = YouTube(url)
-#         # info(yt.streams)
-#         stream = yt.streams.get_by_itag(251)
-#         stream.download(output_path = albumDir, filename = f"{idx}. {yt.title}.raw")
-    
-
-#     # info(f"Fetching {line[0]} album info...")
-#     # playlistInfo = youtube_dl.YoutubeDL({
-#     #     'cookiefile': COOKIES_FILE,
-#     #     'simulate': True,
-#     #     'quiet': False,
-#     # }).extract_info(url, download=False)
-
-#     # cover = urlopen(getCoverUrl(playlistInfo['id'])).read()
-
-#     # for track in playlistInfo["entries"]:
-#     #     options = {
-#     #         'quiet': False,
-#     #         'extractaudio': True,
-#     #         'audioformat': 'webm',
-#     #         'format': 'bestaudio/opus',
-
-#     #         # 'postprocessors': [{
-#     #         #     'key': 'FFmpegExtractAudio',
-#     #         #     'preferredcodec': 'mp3',
-#     #         #     'preferredquality': '320'
-#     #         # }],
-#     #         # 'postprocessor_args': [
-#     #         #     '-ar', '16000'
-#     #         # ],
-#     #         # 'prefer_ffmpeg': True,
-
-#     #         'keepvideo': False,
-#     #         'outtmpl': f"{albumDir}/{track['playlist_index']:02d}."+track['track'].replace('/', ';')+".webm",
-#     #         'cookiefile': COOKIES_FILE
-
-#     #     }
-
-#     #     filename = f"{albumDir}/{track['playlist_index']:02d}." + \
-#     #         track['track'].replace('/', ';')+".mp3"
-
-#     #     if not exists(filename):
-#     #         while True:
-#     #             try:
-#     #                 with youtube_dl.YoutubeDL(options) as ydl:
-#     #                     #ydl.cache.remove()
-#     #                     ydl.download([track['webpage_url']])
-#     #                     break
-#     #             except:
-#     #                 info('Абасрались')
-#     #                 sleep(5)
-#     #                 continue
-#     #         # with youtube_dl.YoutubeDL(options) as ydl:
-#     #         #     ydl.download([track['webpage_url']])
-
-#     #         os.system(
-#     #             'ffmpeg -loglevel 8 -i "' + filename[:-4].replace("\"", "\\\"") + "\".webm -vn -ab 320k -ar 48000 -y \"" + filename.replace("\"", "\\\"")+ '\";')
-#     #         rename(filename, f'{filename}.tmp')
-
-#     #         f = eyed3.load(f'{filename}.tmp')
-#     #         f.tag.album_artist = curArtist
-#     #         f.tag.artist = track['artist']
-#     #         f.tag.track_num = track['playlist_index']
-#     #         f.tag.recording_date = track['release_year']
-#     #         f.tag.title = track['track']
-#     #         f.tag.album = track['album']
-#     #         f.tag.genre = curGenre
-#     #         f.tag.images.set(3, cover, "image/jpeg", u"cover")
-#     #         f.tag.comment = 'fromyoutubemusic'
-
-#     #         f.tag.save()
-
-#     #         rename(f'{filename}.tmp', filename)
-#     #         remove(f"{filename[:-4]}.webm")
-#     #         info(
-#     #             f"{track['playlist_index']:02d}.{track['track']} has been downloaded.")
-#     #     else:
-#     #         info(
-#     #             f"{track['playlist_index']:02d}.{track['track']} has already been downloaded.")
-
-#     # open(f'{albumDir}/.done', 'w').close()
-#     green(f"{line[0]} has been downloaded.")
-#     sleep(15)
-
-
-
-
-
-
-def process_song(filename, album_id, artist_name, album_name, genre, title, year, track_number, total_tracks):
-    cover = urlopen(getCoverUrl(album_id)).read()
+# Set song's metadata
+def process_song(options: SongOptions):
+    cover = urlopen(get_cover_url(options.album_id)).read()
     image = Image.open(io.BytesIO(cover))
 
     with io.BytesIO() as output:
         image.save(output, format="JPEG")
         image_bytes = output.getvalue()
-
-    audio_file = MP4(filename)
+    
+    audio_file = MP4(f'{MUSIC_DIR}/{options.album_foldername}/{options.track_number}. {options.title}.m4a')
 
     audio_file['covr'] = [
         MP4Cover(image_bytes, imageformat=MP4Cover.FORMAT_JPEG)
     ]
 
+
     # Set the artist, album name, and genre
-    audio_file['\xa9ART'] = artist_name  # '\xa9ART' is the tag for the artist
-    audio_file['\xa9alb'] = album_name  # '\xa9alb' is the tag for the album
-    audio_file['\xa9gen'] = genre      # '\xa9gen' is the tag for the genre
-    audio_file['\xa9nam'] = title        # '\xa9nam' is the tag for the title
-    audio_file['\xa9day'] = year      # '\xa9day' is the tag for the release date
-    audio_file['trkn'] = [(track_number, total_tracks)]  # 'trkn' is the tag for the track number
+    if options.title  != "Unknown" and options.title != None:
+        audio_file['\xa9nam'] = options.title                                   # '\xa9nam' is the tag for the title
+
+    if options.artist != "Unknown" and options.artist != None:
+        audio_file['\xa9ART'] = options.artist                                  # '\xa9ART' is the tag for the artist
+
+    if options.album_name:
+        audio_file['\xa9alb'] = options.album_name                              # '\xa9alb' is the tag for the album
+
+    if options.genre != "Unknown" and options.genre != None:
+        audio_file['\xa9gen'] = options.genre                                   # '\xa9gen' is the tag for the genre
+    
+    if options.year:
+        audio_file['\xa9day'] = options.year                                    # '\xa9day' is the tag for the release date
+
+    if options.track_number and options.total_tracks:
+        audio_file['trkn'] = [(options.track_number, options.total_tracks)]     # 'trkn' is the tag for the track number
 
     audio_file.save()
 
-    pass
 
 # Shell Command
 def execute_command(command):
@@ -192,44 +115,41 @@ def execute_command(command):
             print(f"Output: {output.strip()}")
 
         if error:
-            red(f"Error: {error.strip()}")
+            Console.red(f"Error: {error.strip()}")
             exit(1)
 
 
     return process.poll()
 
-def download_song(video_url, folder_path, idx):
+
+# Returns song title
+def get_song_title(song: SongOptions):
     fetch_opts = {
-        'quiet': True,  # Suppress console output
-        'force_generic_extractor': True,  # Use generic extractor for metadata
+        'quiet': True,                      # Suppress console output
+        'force_generic_extractor': True,    # Use generic extractor for metadata
         'cookiefile': COOKIES_FILE,
     }
 
-    song_title = "Unknown"
     with yt_dlp.YoutubeDL(fetch_opts) as ydl:
-        info_dict = ydl.extract_info(video_url, download=False)
-        song_title = info_dict.get('title')
+        info_dict = ydl.extract_info(f'https://www.youtube.com/watch?v={song.id}', download=False)
+        return info_dict.get('title')
+    abort()
 
-    info(f"Downloading {idx}. {song_title} - {video_url}")
 
-    ydl_opts = {
-        'quiet': True,         # Suppress output
-        'cookiefile': COOKIES_FILE,
-        'format': '141',
-        # 'format': 'bestaudio/best[ext=m4a]',
-        'outtmpl': f"{folder_path}/{idx}. {song_title}.m4a",
-    }
+def download_song(song: SongOptions):
+    Console.info(f"Downloading {song.track_number}. {song.title}")
 
     # Hotfix - yt-dlp cant download from music.youtube with ydl.download (music.youtube is needed because of 141 format 256kbs)
-    command_str = f'yt-dlp -f 141 -o "{folder_path}/{idx}. {song_title}.raw" "{video_url}" --cookies="{COOKIES_FILE}" --quiet'
+    command_str = f'yt-dlp -f 141 -o "{MUSIC_DIR}/{song.album_foldername}/{song.track_number}. {song.title}.m4a" "https://music.youtube.com/watch?v={song.id}" --cookies="{COOKIES_FILE}" --quiet'
     execute_command(command_str)
 
-    green(f'{idx}. {song_title} downloaded')
+    Console.green(f'{song.track_number}. {song.title} downloaded')
+
 
 def fetch_playlist_video_urls(playlist_url):
     ydl_opts = {
-        'extract_flat': True,  # Flatten the playlist
-        'quiet': True,         # Suppress output
+        'extract_flat': True,       # Flatten the playlist
+        'quiet': True,              # Suppress output
         'cookiefile': COOKIES_FILE,
     }
     
@@ -240,65 +160,82 @@ def fetch_playlist_video_urls(playlist_url):
             return video_urls
     return []
 
-# name format example In_the_Court_of_the_Crimson_King becomes folder name
-def dl_playlist(playlist_url, playlist_name, artist, genre):
-    album_path = f'{MUSIC_DIR}\\{playlist_name}'
+def get_album_name(song: SongOptions):
+    ydl_opts = {
+        'extract_flat': True,       # Flatten the playlist
+        'quiet': True,              # Suppress output
+        'cookiefile': COOKIES_FILE,
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        result = ydl.extract_info(f'https://www.youtube.com/playlist?list={song.album_id}', download=False)
+        return result.get('title', 'Title not found').split(" - ")[1]
+
+
+def dl_playlist(options: SongOptions):
+    album_path = f'{MUSIC_DIR}/{options.album_foldername}'
     if not exists(album_path):
         mkdir(album_path)
 
-    info (f"Fetching {playlist_name} album info...")
-    video_urls = fetch_playlist_video_urls(playlist_url)
+    Console.info(f"Fetching {options.album_foldername} album info...")
+    video_urls = fetch_playlist_video_urls(f'https://www.youtube.com/playlist?list={options.album_id}')
 
-    info (f"Downloading {playlist_name} [{len(video_urls)} songs]\n")
+    Console.info(f"Downloading {options.album_foldername} [{len(video_urls)} songs]\n")
     if video_urls:
-        for idx, url in enumerate(video_urls, start=1):
-            url = "https://music.youtube.com/watch?v=" + url.split("watch?v=")[1]
-            download_song(url, album_path, idx)
-            process_song()
+        for idx, song_url in enumerate(video_urls, start=1):
+            song = copy.deepcopy(options)
+
+            song.id = song_url.split("/watch?v=")[1]
+            song.title = get_song_title(song)
+            song.track_number = idx
+            song.total_tracks = len(video_urls)
+            song.album_name = get_album_name(song)
+
+            download_song(song)
+            process_song(song)
     else:
-        red("No videos found in the playlist.")
+        Console.red("No videos found in the playlist.")
         return
     with open(f'{album_path}/.done', 'w') as file:
         pass 
-    green(f'{playlist_name} has been downloaded.')
+    Console.green(f'{options.album_foldername} has been downloaded.')
     sleep(10)
 
 
+# True if already downloaded
+def check_playlist_download_state(album_folder):
+    return exists(f'{MUSIC_DIR}/{album_folder}/.done')
+
+def interprete_list(lines):
+    current_artist = "Unknown"
+    current_genre  = "Unknown"
+    
+    for line in lines:
+        line_words = line.split(' ')
+        # Ignore
+        if line_words[0] == '' or line_words[0] == '"': 
+            continue
+        # Set Artist
+        if line_words[0] == "#":
+            current_artist = ' '.join(line_words[1:])
+            continue
+        # Set Genre
+        if line_words[0] == "@":
+            current_genre = ' '.join(line_words[1:])
+            continue
+        
+        # Otherwise download entry
+        if not check_playlist_download_state(line_words[0]):
+            options = SongOptions(album_foldername=line_words[0], album_id=line_words[1], artist=current_artist, genre=current_genre)
+            if len(line_words) >= 3:
+                options.year = line_words[2]
+            dl_playlist(options)
+        else:
+            Console.green(f"{line[0]} is already downloaded.")
+
+
 if __name__ == "__main__":
-    process_song(f'{MUSIC_DIR}/TOHO_BOSSA_NOVA_1/1. furifuri ojousama.m4a', album_id="OLAK5uy_l06YRyzdC4euFmkFiE8zujhFvyVv2GZTQ", artist_name="ShibayanRecords", album_name="TOHO BOSSA NOVA 1", genre="Bossa Nova", title="furifuri ojousama", track_number=1, total_tracks=10, year="2012")
-    # with open(LIST_FILE, 'r') as reader:
-    #     lines = reader.read().split('\n')
-    #     cur_artist = "Unknown"
-    #     cur_genre = "Unknown"
-    #     for linetext in lines:
-    #         line = linetext.split(' ')
-    #         if line[0] == '' or linetext[0] == '"':
-    #             continue
-    #         if line[0] == "#":
-    #             cur_artist = ' '.join(line[1:])
-    #             continue
-    #         if line[0] == "@":
-    #             cur_genre = ' '.join(line[1:])
-    #             continue
+    with open(LIST_FILE, 'r') as reader:
+        lines = reader.read().split('\n')
+        interprete_list(lines)
 
-    #         if not exists(f'{MUSIC_DIR}/{line[0]}/.done'):
-    #             dl_playlist(f'https://www.youtube.com/playlist?list={line[1]}', line[0], cur_artist, cur_genre)
-    #         else:
-    #             green(f"{line[0]} is already downloaded.")
-
-
-
-# driver = webdriver.Firefox()
-# d = driver.get('https://music.youtube.com/search?q=TOHO_BOSSA_NOVA_3')
-# print(d)
-# driver.quit()
-# with urlopen(f'https://music.youtube.com/search?q=TOHO_BOSSA_NOVA_3') as res:
-#     body = res.read().decode('utf-8')
-#     print(body)
-    # key = '<meta property="og:image" content="'
-    # body = body[body.index(key)+len(key):]
-    # body = body[:body.index('">')]
-
-# f = eyed3.load('/home/tuma/Music/TOHO_BOSSA_NOVA_1/01.furifuri ojousama.mp3')
-# f.tag.album_artist = 'd'
-# f.tag.save()
+# process_song(f'{MUSIC_DIR}/TOHO_BOSSA_NOVA_1/1. furifuri ojousama.m4a', album_id="OLAK5uy_l06YRyzdC4euFmkFiE8zujhFvyVv2GZTQ", artist_name="ShibayanRecords", album_name="TOHO BOSSA NOVA 1", genre="Bossa Nova", title="furifuri ojousama", track_number=1, total_tracks=10, year="2012")
